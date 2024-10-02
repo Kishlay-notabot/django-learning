08ef5a02a558175386712977fd8ad2b5f7d9a8c3
kishlay-notabot <kishlay.work1@gmail.com>
2024-10-02 21:05:02 +0530

    add getobject or 404 shortcut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Ques
 
@@ -8,10 +8,7 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    try:
-        question = Ques.objects.get(pk=question_id)
-    except Ques.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Ques, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 def results(request, question_id):
     response = "You're looking at the results of question %s."
