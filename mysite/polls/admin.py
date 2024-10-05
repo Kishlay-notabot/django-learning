164c0ad64bfd228b94505d10e6f8d33934674f17
kishlay-notabot <kishlay.work1@gmail.com>
2024-10-05 12:51:34 +0530

    import choice model in admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Ques
+from .models import Ques, Choice
 
 admin.site.register(Ques)
 admin.site.register(Choice)
