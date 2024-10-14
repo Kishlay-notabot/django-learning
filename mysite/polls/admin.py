aef4676f930c88ca2edc03f3bf611abdb3d880f4
kishlay-notabot <kishlay.work1@gmail.com>
2024-10-14 16:20:05 +0530

    improve choices form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Ques
 
-from .models import Ques, Choice
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 admin.site.register(Ques, QuestionAdmin)
-admin.site.register(Choice)
