cef00894f0d3523a46b185affd672493e8d9b18a
kishlay-notabot <kishlay.work1@gmail.com>
2024-10-15 13:04:34 +0530

    add filter and search field, tutorial complete

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -15,4 +15,6 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 admin.site.register(Ques, QuestionAdmin)
